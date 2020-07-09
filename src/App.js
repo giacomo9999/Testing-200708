f9898c8cb18ae406b966a02eefc6b839ee0f2e77
giacomo9999 <gmarchesi888@gmail.com>
2020-07-09 15:13:02 -0400

    Added styling.

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 import GuessedWords from "./GuessedWords";
 import Congrats from "./Congrats";
 
-import "./App.css";
+
 
 class App extends Component {
   render() {
